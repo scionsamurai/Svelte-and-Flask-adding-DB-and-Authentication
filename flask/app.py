e218497275fa43a6c44b68e43f8921c76f7883cc
scionsamurai <scionsamurai@yahoo.com>
2023-08-15 19:36:34 -0500

    Added base login functions

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, request
 from flask_login import login_user, LoginManager, logout_user, login_required
 from flask import render_template
 import json
 
+from utils import redirect_back
+from sql_utils import User
+
 app = Flask(__name__)
 
-# login_manager = LoginManager(app)
-# login_manager.init_app(app)
+app.secret_key = "sassdfsdfs3sdfdfdadsf2423442sdfasdf2fb3443b4"
+login_manager = LoginManager(app)
+login_manager.init_app(app)
 
 @app.route("/")
+@login_required
 def home():
     return render_template('svelte_main.html', page="index", extra_data=json.dumps({'current_user': "insert user check function here", 'test': "This is a test",}))
 
@@ -19,6 +24,36 @@ def login_page():
 @app.route("/signup")
 def signup_page():
     return render_template('svelte_main.html', page="signup", extra_data=json.dumps({'current_user': "Want to be a user?", 'test': "This is another test",}))
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        name = request.form["username"]
+        password = request.form["password"]
+        user = User(name, password)
+        if user.is_authenticated():
+            login_user(user, remember=True)
+            return redirect_back('home')
+        else:
+            return render_template('svelte_main.html', page='login')
+    else:
+        return render_template('svelte_main.html', page='login')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    # do stuff
+    return redirect('/login')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
---
+++ flask/sql_utils.py
@@ -0,0 +1,70 @@
+import psycopg2
+from db.hide import variables
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=variables.your_database_name,
+        user=variables.your_username,
+        password=variables.your_password,
+        host=variables.your_host,
+        port=variables.your_port
+    )
+    return conn
+    
+class User:
+    name = ""
+    password = ""
+
+    def __init__(self, name="", password=""):
+        self.name = name
+        self.password = password
+        self.id = 0
+
+    def is_authenticated(self):
+        query = 'SELECT * FROM "User" WHERE username = %s;'
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(query, (self.name,))
+        data = cur.fetchone()
+        print('data', data)
+        cur.close()
+        conn.close()
+        if data is None:
+            return False
+        else:
+            print("self.check_password(data[1])", self.check_password(data[1]))
+            if self.check_password(data[1]):
+                return True
+            else:
+                return False
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return str(self.name)
+
+    def set_password(self, passwordy):
+        self.pw_hash = passwordy
+
+    def check_password(self, passwordx):
+        return passwordx == self.password
+
+    @staticmethod
+    def get(user_name):
+        query = 'SELECT * FROM "User" WHERE username = %s;'
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(query, (user_name,))
+        data = cur.fetchone()
+        user = User()
+        if data:
+            user.name = data[0]
+            cur.close()
+            conn.close()
+            return user
+        else:
+            cur.close()
+            conn.close()
+            return None
+
